2bfcbade811ae121d23c3db31ca9c0cceeac9bc6
IldarZay <zayildar@gmail.com>
2024-06-27 23:59:26 +0400

    Добавление проекта на зачет

---
+++ src/test/java/Checkboxes.java
@@ -0,0 +1,29 @@
+package src.test.java;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class Checkboxes {
+
+    @Test
+    public void checkboxTest(){
+
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+        WebDriver driver = new ChromeDriver(options);
+
+        driver.get("http://the-internet.herokuapp.com/ ");
+
+
+
+
+
+    }
+
+
+
+}
